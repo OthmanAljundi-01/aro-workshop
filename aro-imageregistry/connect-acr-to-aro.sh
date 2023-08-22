d69c958914d17461480f87b0e20a58c578a5630c
Othman AlJundi <59793292+OthmanAljundi-01@users.noreply.github.com>
2023-08-22 12:35:36 +0300

    Create connect-acr-to-aro.sh

---
+++ aro-imageregistry/connect-acr-to-aro.sh
@@ -0,0 +1,71 @@
+# The following commands will be creating ACR then configure pull secret in the ARO Cluster
+
+
+
+
+EngAlias=`az account show --query user.name -otsv | cut -d"@" -f1`
+
+ACRName="aroacr$EngAlias"
+ACRRG="aro-acr-$EngAlias-rg"
+
+
+az group create --name $ACRRG --location westeurope
+
+az acr create --resource-group $ACRRG --name $ACRName --sku Premium --admin-enabled true --location westeurope --public-network-enabled
+
+az acr list -o table
+
+
+az acr credential show -n $ACRName -g $ACRRG -ojson > acrcredential.json
+
+
+az acr login --name $ACRName
+
+docker pull nginx:1.8
+docker tag docker.io/library/nginx:1.8 $ACRName.azurecr.io/nginx:1.8
+docker push $ACRName.azurecr.io/nginx:1.8
+docker images | grep -i $ACRName
+
+
+az acr repository list --name $ACRName --output table
+
+
+DockerUsername=`jq .username acrcredential.json -r`
+DockerPassword=`jq .passwords[0].value acrcredential.json -r`
+
+
+oc create secret docker-registry --docker-server=$ACRName.azurecr.io --docker-username=$DockerUsername --docker-password=$DockerPassword --docker-email=unused acr-secret
+
+oc get secrets acr-secret
+
+oc get secrets acr-secret -oyaml
+
+
+cat > acrpod1.yaml<< EOF
+apiVersion: v1 
+kind: Pod 
+metadata: 
+  labels: 
+     app: nginx 
+  name: nginx 
+spec: 
+  containers: 
+  - name: nginx 
+    image: $ACRName.azurecr.io/nginx:1.8
+    ports: 
+    - containerPort: 80 
+  imagePullSecrets:
+  - name: acr-secret
+EOF
+
+ls acrpod1.yaml
+cat acrpod1.yaml
+
+oc apply -f acrpod1.yaml
+
+oc get pods nginx -w
+
+
+
+
+
